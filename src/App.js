ea2f3f047ce95a28560351359bb74a56a5925fb0
RichardDufour <richarddufour@laposte.net>
2022-02-10 14:43:31 +0100

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          TEST TEST
+          TEST TESTgrzgzrg
         </a>
       </header>
     </div>
